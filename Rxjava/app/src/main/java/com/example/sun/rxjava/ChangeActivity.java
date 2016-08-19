47f24f6c81e1c5842df6b7d16f99be91898a2656
langsun <lang_un@sina.com>
2016-08-19 12:29:54 +0800

    添加变换操作

---
+++ Rxjava/app/src/main/java/com/example/sun/rxjava/ChangeActivity.java
@@ -0,0 +1,15 @@
+package com.example.sun.rxjava;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by sun on 16/8/19.
+ */
+public class ChangeActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change);
+    }
+}
